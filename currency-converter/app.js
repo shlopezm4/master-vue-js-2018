3d48636239bca6829b108f5cde1d3c3b2822dcaa
Humberto López Menéndez <slopezm14@gmail.com>
2019-11-18 16:24:58 -0600

    Caching currencies to limit api requests

--- currency-converter/app.js
+++ currency-converter/app.js
@@ -3,10 +3,22 @@ new Vue({
     data: {
         currencies: {}
     },
+    //mounted → Es un hook que permite que cuando se cargue la app
+    // haga lo que se le está indicando
+    // ver más en: https://es.vuejs.org/v2/guide/instance.html#Hooks-del-Ciclo-de-vida-de-la-Instancia
     mounted() {
-        axios.get('https://free.currconv.com/api/v7/currencies?apiKey=sample-key-do-not-use')
-            .then(response => {
-                this.currencies = response.data.results;
-            })
+        this.getCurrencies();
+    },
+    methods: {
+        getCurrencies() {
+            const currencies = localStorage.getItem('currencies');
+            axios.get('https://free.currconv.com/api/v7/currencies?apiKey=sample-key-do-not-use')
+                .then(response => {
+                    this.currencies = response.data.results;
+
+                    //Guardar en localStorage
+                    localStorage.setItem('currencies', JSON.stringify(response.data.results));
+                })
+        }
     }
 })
