487c018d1b98dcef7f56646346a5c4319c14763c
Humberto López Menéndez <slopezm14@gmail.com>
2019-11-20 11:35:54 -0600

    Currencies to the select list

--- currency-converter/app.js
+++ currency-converter/app.js
@@ -9,9 +9,19 @@ new Vue({
     mounted() {
         this.getCurrencies();
     },
+    computed: {
+        formattedCurrencies() {
+            return Object.values(this.currencies);
+        }
+    },
     methods: {
         getCurrencies() {
             const currencies = localStorage.getItem('currencies');
+            if (currencies) {
+                this.currencies = JSON.parse(currencies);
+                return;
+            }
+
             axios.get('https://free.currconv.com/api/v7/currencies?apiKey=sample-key-do-not-use')
                 .then(response => {
                     this.currencies = response.data.results;
